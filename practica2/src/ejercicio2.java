e056e1b65472222227baf3cd67352464cc332e73
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-16 16:14:58 +0200

    ejercicio2 terminado

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,12 @@
+
+public class ejercicio2 {
+    static void main(String[] args){
+        // Muestra por pantalla todos los elementos de un array de
+        // números enteros separados por un espacio
+        // Realiza un array con diferentes valores
+        int[] array = {10,2,5,7,4,1,67,9,3,8};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" "+array[i]);
+        }
+    }
+}
