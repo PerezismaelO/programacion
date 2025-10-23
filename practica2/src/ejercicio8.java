71b86ff72e77d5ad8594984a501fdaefca3a57f9
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 01:29:10 +0200

    ejercicio8 terminado

---
+++ practica2/src/ejercicio8.java
@@ -0,0 +1,48 @@
+import java.util.Scanner:
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        // Programa Java para leer la altura de N personas y calcular la altura media. Calcular cuántas
+        // personas tienen una altura superior a la media y cuántas tienen una altura inferior a la
+        // media. El valor de N se pide por teclado y debe ser entero positivo.
+        Scanner s = new Scanner(System.in);
+        int N;
+
+        // El número de personas
+        System.out.print("Introduce el número de personas (N > 0): ");
+        N = s.nextInt();
+        if (N <= 0) {
+            System.out.println("Error: N debe ser un número entero positivo.");
+            }
+        while (N <= 0);
+        double[] array = new double[N];
+        double suma = 0;
+
+        // Leer las alturas
+        System.out.println("Introduce las alturas: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Altura de la persona " + (i + 1) + ": ");
+            alturas[i] = sc.nextDouble();
+            suma += alturas[i];
+        }
+
+        // Calcular la media
+        double media = suma / N;
+        System.out.println("La altura media es:" media);
+
+        // Contar cuántas están por encima y por debajo
+        int superiores = 0, inferiores = 0;
+
+        for (double altura : alturas) {
+            if (altura > media) {
+                superiores++;
+            } else if (altura < media) {
+                inferiores++;
+            }
+        }
+        System.out.println("Personas con altura superior a la media: " + superiores);
+        System.out.println("Personas con altura inferior a la media: " + inferiores);
+        sc.close();
+
+    }
+}
