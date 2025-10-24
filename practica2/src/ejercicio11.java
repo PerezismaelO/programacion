c38d043eb51691adc8f8d610de13f6792a520dd1
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 16:26:21 +0200

    ejercicio11 terminado

--- practica2/src/ejercicio11.java
+++ practica2/src/ejercicio11.java
@@ -7,32 +7,32 @@ public class ejercicio11 {
         // me refiero que el último pasa a ser el primero, el
         // penúltimo el segundo y así sucesivamente. PRUEBA CON UN ARRAY DE TAMAÑO 6
         Scanner s = new Scanner(System.in);
+        // Inicializamos el array de tamaño 6
         int[] array = new int[6];
-
+        // Pedimos 6 valores
         System.out.println("Introduce 6 números:");
+        // Recorremos el array
         for (int i = 0; i < array.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             array[i] = s.nextInt();
         }
-
-        // Mostrar el array original
+        // Mostramos el array original para despues poder invertir el orden de los elementos
         System.out.println("Array original:");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
 
-        // Invertir el orden de los elementos
+        // Invertimos el orden de los elementos
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = temp;
         }
 
-        // Mostrar el array invertido
+        // Mostramos el array invertido
         System.out.println("Array invertido:");
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
-        s.close();
     }
 }
