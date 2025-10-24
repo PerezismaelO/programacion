2b0bb2f8c2b066190b1aeb69dcc4ce6990a62e6b
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 16:09:21 +0200

    ejercicio7 terminado

--- practica2/src/ejercicio7.java
+++ practica2/src/ejercicio7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ejercicio7 {
     public static void main(String[] args) {
-        // Programa Java que llene un array con 10 números enteros que se leen por teclado. A
+        // Programa Java que llene un array con 10 numeros enteros que se leen por teclado. A
         // continuación calcula y muestra la media de los valores positivos y la de los valores negativos
         // del array.
         Scanner s = new Scanner(System.in);
