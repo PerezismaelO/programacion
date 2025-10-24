ae9ae12e0add115754e91f3e58a09203f5f98611
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 16:48:12 +0200

    ejercicio14 algo falla

--- practica2/src/ejercicio14.java
+++ practica2/src/ejercicio14.java
@@ -9,8 +9,37 @@ public class ejercicio14 {
         // Luego pida un nombre a buscar y diga si está en la lista o no, mostrando también en
         // qué posición se encuentra si existe.
         Scanner s = new Scanner(System.in);
-        ArrayList<Integer> listaNombres = new ArrayList<>();
+        ArrayList<String> nombres = new ArrayList<>();
+        String entrada;
+        String nombre;
 
+        // Pedimos nombres hasta escribit "fin"
+        System.out.println("Introduce un nombre:");
+        do {
+            System.out.println("Nombre: ");
+            entrada = s.nextLine().trim();
+            if (!entrada.equals("fin")) {
+                nombres.add(entrada);
+            }
+        } while (!entrada.equals("fin"));
 
+        // Recorremos la lista con un for each
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+        // Buscamos un nombre, si está en el Arraylist, devuelve "está en la lista"
+        // , y si no pues devuelve que no está.
+        System.out.print("Introduce un nombre a buscar: ");
+        String nombreBuscado = sc.nextLine().trim();
+
+        if (nombres.contains(nombreBuscado)) {
+            int posicion = nombres.(nombreBuscado);
+            System.out.println("El nombre '" + nombreBuscado + "' está en la lista en la posicion " + posicion);
+        } else {
+            System.out.println("El nombre '" + nombreBuscado + "' no se encuentra en la lista.");
+        }
     }
 }
+
+
+
