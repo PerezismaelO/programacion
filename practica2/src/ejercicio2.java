abaccda53c21784b21c052e9309b06c8f681e8f3
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 15:27:27 +0200

    ejercicio2 terminado

--- practica2/src/ejercicio2.java
+++ practica2/src/ejercicio2.java
@@ -2,9 +2,13 @@
 public class ejercicio2 {
     static void main(String[] args){
         // Muestra por pantalla todos los elementos de un array de
-        // números enteros separados por un espacio
+        // numeros enteros separados por un espacio
         // Realiza un array con diferentes valores
+
+        // Inicializamos el array y le damos valores
         int[] array = {10,2,5,7,4,1,67,9,3,8};
+
+        // Recorremos el array para que los escriba con un espacio entre cada valor
         for (int i = 0; i < array.length; i++) {
             System.out.print(" "+array[i]);
         }
