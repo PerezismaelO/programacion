10d0d375ab3f41c1699766ecf5c332affda4ddf5
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-24 16:32:50 +0200

    ejercicio13 algo falla

--- practica2/src/ejercicio13.java
+++ practica2/src/ejercicio13.java
@@ -8,11 +8,13 @@ public class ejercicio13 {
         // no se añade).
         // Muestre por pantalla todos los números de la lista y la suma total de los mismos.
         Scanner s = new Scanner(System.in);
+        // Inicializamos el array con un tamaño cualquiera
         int[] array = new int[1000];
         int numero;
         int contadorNum = 0;
         int sumaNum = 0;
 
+        // Recorremos el array
         for (int i = 0; i < array.length; i++) {
             System.out.println("Introduce un número: ");
             numero = s.nextInt();
@@ -21,16 +23,14 @@ public class ejercicio13 {
             sumaNum += numero;
             if (numero < 0) {
                 break;
-
             }
         }
 
-
         System.out.println("Los números introducidos son:");
+        // Recorremos otra vez el array para contar los números positivos y sumarlos
         for (int i = 0; i < contadorNum; i++) {
             System.out.println(array[i]);
         }
-
         System.out.println("La suma total es: " + sumaNum);
     }
 }
