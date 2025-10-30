b295d74a1bc02a8e488b7ac54ff4526e671c70a3
PerezismaelO <iperezdelabarrera@gmail.com>
2025-10-30 14:14:56 +0100

    Agenda

---
+++ practica3/src/Agenda.java
@@ -0,0 +1,6 @@
+public class Agenda {
+    public static void main(String[] args) {
+        // Controlar codigo binario y saber como pasar decimales a binario.
+        // Terminar entorno
+    }
+}
