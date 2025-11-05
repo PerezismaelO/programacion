460582bd1c213caea47f28a93c8d7d3dc63918d4
PerezismaelO <iperezdelabarrera@gmail.com>
2025-11-05 14:26:41 +0100

    ejercicio2

--- practica3/src/ejercicio2.java
+++ practica3/src/ejercicio2.java
@@ -16,12 +16,36 @@ public class ejercicio2 {
                 array[i][j] = s.nextInt();
             }
         }*/
-        int [][] array = {
+        int[][] array = {
                 {2, 5, 3, 6, 7},
                 {7, 4, 3, 2, 45},
                 {6, 63, 2, 4, 6},
                 {3, 5, 6, 7, 3}
         };
+        int total = 0;
+        for (int i = 0; i < array.length; i++) { // Recorre las filas
+            int sumaFila = 0;
+            for (int j = 0; j < array[i].length; j++) { // Recorre las columnas
+                sumaFila += array[i][j];
+                System.out.printf("  " + " %-7d ", array[i][j]);
+            }
+            total += sumaFila;
+            System.out.printf("Fila %d Suma = %-6d\n", i, sumaFila);
+            System.out.println();
+        }
 
+
+        for (int j = 0; j < array[0].length; j++) { // Recorre las filas
+            int mayor = array[0][j];
+            for (int i = 0; i < array.length; i++) { // Recorre las columnas
+
+                if (array[i][j]>mayor) {
+                    mayor = array[i][j];
+
+                }
+            }
+            System.out.printf("   %-8d", mayor);
+        }
+        System.out.println("Total= " + total);
     }
 }
