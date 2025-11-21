430ca00b3e2bec434caa1069c8fa3efd17152b43
PerezismaelO <iperezdelabarrera@gmail.com>
2025-11-21 10:48:54 +0100

    practica4 creada

---
+++ practica4/src/Direccion.java
@@ -0,0 +1,12 @@
+public class Direccion {
+
+    // atributos
+    private static String calle;
+    private static int numero;
+    private static int codigo_postal;
+    private static String localidad;
+
+
+
+
+}
